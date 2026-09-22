a66f55525e35ee4102d009546440d6fb6084f350
Denberg28 <111175467+Denberg28@users.noreply.github.com>
2026-09-22 15:28:28 +0800

    Bump HARU to v0.9.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "io.haru.assistant"
         minSdk = 26
         targetSdk = 36
-        versionCode = 40
-        versionName = "0.9.5"
+        versionCode = 41
+        versionName = "0.9.6"
 
         ndk {
             abiFilters += "arm64-v8a"
